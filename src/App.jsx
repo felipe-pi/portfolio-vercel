be0be2430e8d15845e8f515ae23230217126d36f
feliperamos <felipe.pi@outlook.com>
2026-04-16 13:54:18 -0300

    Ajuste no arquivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/header';
+import Header from './components/Header';
 import Sobre from './components/Sobre';
 import Projetos from './components/Projetos';
 import './App.css';
